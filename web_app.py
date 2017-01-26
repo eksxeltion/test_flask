71a4956333b249795e335646eb2fad24ecba2a2e
Stephane Lo <stephane@stephane-mbp13-rl1gg.lan>
2017-01-25 23:37:33 -0800

    fixing the indent

--- web_app.py
+++ web_app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	place = request.values.get('place')
-	if place:
-		return render_template('index.html', place=place, weather=weather.get_weather(place))
-	else:
-		return render_template('index.html')
+    place = request.values.get('place')
+    if place:
+        return render_template('index.html', place=place, weather=weather.get_weather(place))
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0", port=port)
